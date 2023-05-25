3ecf16b33c7160fe1dd863be5b08bd0812d2adb8
emmamarks <37787980+emmamarks@users.noreply.github.com>
2023-05-25 16:42:48 +0100

    Add files via upload

---
+++ src/components/Screens/Home.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import axios from 'axios';
+
+function Home({ history }) {
+
+    useEffect(() => {
+        if(!localStorage.getItem('authToken')){
+            history.push('/')
+        }
+    })
+ 
+    const [username, setUsername] = useState('');
+    const authToken = localStorage.getItem('authToken');
+
+    const config = {
+        headers:{
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`
+        }
+    }
+
+    const getUserProfile = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/home`, config)
+
+            setUsername(response.data.username)
+            
+        } catch (error) {
+            localStorage.removeItem('authToken');
+            history.push('/')
+        }        
+    }
+    
+    useEffect(() => {
+        getUserProfile()
+    }, [])
+
+
+    function handleClick(){
+        localStorage.removeItem('authToken');
+        localStorage.removeItem('email');
+        history.push('/')
+    }
+
+    return(
+        <div>
+            <h1>
+                Betty Cash
+            </h1>
+            <br />
+            Welcome <Link>{username}</Link> <br /> <br />
+            <button onClick={(e) => handleClick()} className="btn">Logout</button><br /> <br />
+        </div>
+    );
+}
+
+export default withRouter(Home);
---
+++ src/components/Screens/Login.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import { Link, withRouter } from "react-router-dom";
+import axios from 'axios';
+
+function Login({ history }) {
+ 
+    // useEffect(() =>{
+    //     if(localStorage.getItem('authToken')){
+    //         history.push('/home');
+    //     }
+    // }, [ history ]);
+    
+    const [input, setInput] = useState({
+        username:'',
+        password:''
+    })
+
+    const [error, setError] = useState('');
+
+
+    function handleChange(event) {
+        const{name, value} = event.target;
+
+        setInput(prevInput =>{
+            return{
+                ...prevInput,
+                [name]: value
+            }
+        })
+    }
+
+    async function handleClick(event){
+        event.preventDefault();
+        try {    
+            if(input.username && input.password){
+    
+                const registered = {
+                    username:input.username,
+                    password:input.password
+                }
+                
+                const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, registered)
+            
+                localStorage.setItem('authToken', response.data.token);
+                            
+                history.push('/home');
+    
+            }else{
+                return setError("Username & Password required")
+            }
+        } catch (error) {
+            return setError(error.response.data.error)
+        }          
+    }
+
+    return <div>
+        <h1>
+            Betty Cash
+        </h1> 
+        {error && <span>{error}</span>}<br /><br />
+        Username: <input type="text" placeholder ="Username"
+        name="username" id="username" value={input.username} onChange={handleChange} /><br /><br />
+        Password: <input type="password" placeholder ="Enter Password"
+        name="password" id="password" value={input.password} onChange={handleChange} /><br /> <br />
+        <small><Link to= "/forgot">Forgot Password ?</Link></small><br /> <br />
+        <button onClick={(e) => handleClick(e)} className="btn">Login</button><br /> <br />
+        <small>
+        <Link to= "/send">New Here? Sign Up </Link><br /> <br />
+        © Jacob 2023 
+        </small>
+    </div>
+}
+
+export default withRouter(Login);
