ca6138ff15b9de702c6377efe7484e377cf9850e
emmamarks <37787980+emmamarks@users.noreply.github.com>
2023-07-04 17:00:33 +0100

    Add files via upload

---
+++ Pay.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { Link, withRouter, useParams } from "react-router-dom";
+import { usePaystackPayment } from "react-paystack";
+import axios from 'axios';
+
+const Paystack = ({ history }) => {
+
+  const {_id} = useParams();
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const [email, setEmail] = useState('');
+  const [predictions, setPredictions] = useState([]);
+  const params = useParams();
+
+  const [config, setConfig] = useState({
+		// email: "decgds22@gmail.com",
+		// amount: "345322",
+		email: localStorage.getItem("email"),
+		amount: localStorage.getItem("amount"),
+		// publicKey: process.env.REACT_APP_PAYSTACK_PUBLIC_KEY,
+		publicKey: "pk_live_2ca52b3edabd7cf5169058f64aede8d4adce1822"
+	});
+
+	// First initialization of the Library
+	const initializePayment = usePaystackPayment(config);
+
+	// Callback if transaction is successful
+	const onSuccess = () => {
+		alert("Payment Successful, check your email for confirmation");
+	};
+
+	// Callback if payment gateway is closed
+	const onClose = () => {
+		alert("Opps, Payment not completed");
+	};
+	const handleClick = (e) => {
+		// Trigger Payment Gateway on Form Submit
+		initializePayment(onSuccess, onClose);
+		localStorage.removeItem('email');
+		localStorage.removeItem('amount');
+	};
+
+	const authToken = localStorage.getItem('authToken');
+  const confi = {
+    headers:{
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`
+    }
+  }
+
+  useEffect(() => {
+    getPredictions()
+  }, [])
+
+  const getPredictions = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/pay/${_id}`, confi)
+      setDescription(response.data.result.description)
+      setAmount(response.data.result.amount)
+      setEmail(response.data.result.email)
+    } catch (error) {
+      localStorage.removeItem('authToken');
+      history.push('/')
+    } 
+  }
+
+	return (
+		<>
+			<div className="container">
+				Amount: ₦{amount / 100}<br /> <br />
+				Description: {description}<br /> <br />
+				Email: {email}<br /> <br />
+				<div className="row mt-5">
+					<div className="col-sm-4 mx-auto my-form text-center">							
+							<div>
+              					<Link to = '/home'><button className="btn">Go Back</button></Link>
+								<button onClick={(e) => handleClick()} className="btn">Pay Now</button>
+							</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Paystack;
